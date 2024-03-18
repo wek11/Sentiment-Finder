4303e8ed571878ca652e363c6a05e19c6977beae
Will <wek1122@gmail.com>
2024-03-18 08:47:33 -0700

    commit

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def getHTML(link):
 
 
 def filter_tags(text):
-    filtered_text = []
-    unwanted_tags = {"none;", "\"locator", "{\""}
-    indiv_tag = ""
+    filtered_text = list = []
+    unwanted_tags = set = {"none;", "\"locator", "{\"", "\"type\\"}
+    indiv_tag: str = ""
 
     # Doesn't append tags containing data in the unwanted_tags set #
     # and also stops appending tags past the copyright (near the end of the article) #
@@ -37,19 +37,15 @@ def filter_tags(text):
 
             indiv_tag = indiv_tag.strip(" ")
 
-            """
-            Missing two of the tags at the start of the article, think it might be with locator in unwanted_tags
-            """
 
             # Doesn't append tag if its not a paragraph
-            if indiv_tag[0].isupper():
+            if indiv_tag[0].isupper() or indiv_tag[0] == "\"":
                 # Adds the first 15 characters of each tag appended to sorted_text
                 # to unwanted_tags to avoid duplicate text blocks
                 # Adds b instead of indiv_tag b/c data.txt is not filtered
-                unwanted_tags.add(b[:15])
+                unwanted_tags.add(b[:len(b) - 10])
 
                 filtered_text.append(indiv_tag)
-
 
 
     """
@@ -82,20 +78,14 @@ def filter_tags(text):
     return filtered_text
 
 def parse_data(data):
-    remove_rest = False
-    remove_list = []
-    append_data = False
-    text = []
-    parsed_data = data.split('":')
-    # print(parsedData)
+    append_data = boolean = False
+    text = list = []
+    parsed_data = list = data.split('":')
     for i in parsed_data:
         if append_data:
-            # print(i)
             text.append(i)
-            # print(text)
         if "text" in i:
             append_data = True
-            # print(True)
         else:
             append_data = False
 
@@ -106,18 +96,7 @@ def parse_data(data):
 
     text = filter_tags(text)
 
-
-
-
-
-
-
-
-
-
-
     return text
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -130,7 +109,7 @@ if __name__ == '__main__':
     test_data = open("testData.txt", 'r+', encoding="utf-8")
 
     if test_data.read() == "":
-        rawData = requests.get('https://www.bbc.com/news/world-europe-68543919', headers=headers, verify=False).text
+        rawData = requests.get('https://www.bbc.com/news/world-europe-68596231', headers=headers, verify=False).text
         print(rawData)
         test_data.write(rawData)
 
